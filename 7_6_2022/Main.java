72e284ecca55f7adde6734552470bc6b99e21956
Samrat Bhardwaj <bsamratv@amazon.com>
2022-06-07 22:01:16 +0530

    Two Pointers

--- 7_6_2022/Main.java
+++ 7_6_2022/Main.java
@@ -53,10 +53,47 @@ public class Main {
             }
         }
         
-        
-        
         return count;
     }
+
+    // merge two sorted arrays ========================================================= 
+
+    // O(n+m)
+    public static int[] merge(long arr1[], long arr2[], int n, int m) 
+    {
+        long[] ans=new long[n+m];
+        
+        int i=0;
+        int j=0;
+        int k=0;
+        
+        while(i<n && j<m){
+            if(arr1[i]<arr2[j]){
+                ans[k]=arr1[i];
+                i++;
+                k++;
+            } else {
+                ans[k]=arr2[j];
+                j++;
+                k++;
+            }
+        }
+        
+        while(i<n){
+            ans[k]=arr1[i];
+            i++;
+            k++;
+        }
+        
+        while(j<m){
+            ans[k]=arr2[j];
+            j++;
+            k++;
+        }
+        
+        return ans;
+    }
+    
     public static void main(String[] args) {
         
     }
