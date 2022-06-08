468d8bd8ea5ac9a4073cbd522d3015d923cc1f9d
Samrat Bhardwaj <bsamratv@amazon.com>
2022-06-08 21:46:23 +0530

    Update till 8 june

--- 8_6_2022/TwoPointer.java
+++ 8_6_2022/TwoPointer.java
@@ -45,6 +45,36 @@ public class TwoPointer {
         return count;
     }
 
+    public static int countTriplet(int[] nums, int target){
+        Arrays.sort(nums);
+        int n=nums.length;
+
+        int count=0;
+
+        for(int k=0; k<n-2; k++){
+            int i=k+1;
+            int j=n-1;
+
+            int required_sum=tar-nums[k];
+
+            while(i<j){
+                int sum=nums[i] + nums[j];
+
+                if(sum==required_sum){
+                    count++;
+                    i++;
+                    j--;
+                } else if(sum<required_sum){
+                    i++;
+                } else {
+                    j--;
+                }
+            }
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
         int[] nums=new int[]{-1,11,-2,5,13,9,4,-4,15};
         int target=9;
