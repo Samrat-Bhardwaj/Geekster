7c74191571fc5ae36672f6c5f84c9d97be79c6a4
Samrat Bhardwaj <bsamratv@amazon.com>
2022-06-09 21:58:30 +0530

    Till pivot index

--- 9_6_2022/PrefixSum.java
+++ 9_6_2022/PrefixSum.java
@@ -1,6 +1,36 @@
 import java.util.*;
 
 public class PrefixSum {
+    // leet 724 ========================================= 
+    public int pivotIndex(int[] nums) {
+        int n=nums.length;
+
+        int[] pre=new int[n];
+
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum=sum+nums[i];
+
+            pre[i]=sum;
+        }
+
+        for(int i=0; i<n; i++){
+            int left_sum;
+            if(i==0){
+                left_sum=0;
+            } else {
+                left_sum=pre[i-1];
+            }
+            
+            int right_sum=pre[n-1]-pre[i];
+
+            if(left_sum==right_sum){
+                return i;
+            }
+        }
+
+        return -1;
+    }
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
 
@@ -26,7 +56,7 @@ public class PrefixSum {
         int x=scn.nextInt();
         int y=scn.nextInt();
 
-        int range_sum=0; // sum between index x and y
+        int range_sum; // sum between index x and y
 
         if(x==0){
             range_sum=pre[y] - 0; 
