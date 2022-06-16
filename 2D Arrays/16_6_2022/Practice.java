7210091af4f013b42da7974faa51f589bd408ce8
Samrat Bhardwaj <bsamratv@amazon.com>
2022-06-16 20:52:33 +0530

    Identity

--- 2D Arrays/16_6_2022/Practice.java
+++ 2D Arrays/16_6_2022/Practice.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Practice {
     public static boolean checkIdentityMatrix(int[][] arr, int n, int m){
         if(n!=m){
@@ -21,6 +23,18 @@ public class Practice {
         return true;
     }
     public static void main(String[] args) {
-        
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int m=scn.nextInt();
+
+        int[][] arr=new int[n][m];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                arr[i][j]=scn.nextInt();
+            }
+        }
+
+        System.out.println(checkIdentityMatrix(arr,n,m));
     }
 }
