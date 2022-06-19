b59768d87eb29b5caba4b017337643e42c19d521
Samrat Bhardwaj <bsamratv@amazon.com>
2022-06-19 12:04:15 +0530

    leetcode 11

---
+++ 19_6_2022/Leet11.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/container-with-most-water/ 
+
+public int maxArea(int[] height) {
+    int n=height.length;
+    int i=0;
+    int j=n-1;
+    
+    int max_area=0;
+    
+    while(i<j){
+        int height_tower=Math.min(height[i],height[j]);
+        int width=j-i;
+        
+        int curr_area=height_tower*width;
+        
+        if(max_area<curr_area){
+            max_area=curr_area;
+        }
+        
+        if(height[i]<height[j]){
+            i++;
+        } else {
+            j--;
+        }
+    }
+    
+    return max_area;
+}
