b9bb653b0a7278715d9c42c6450e86374ef19330
Samrat Bhardwaj <bsamratv@amazon.com>
2022-06-24 22:01:36 +0530

    update

--- Strings/24_6_2022/Practice.java
+++ Strings/24_6_2022/Practice.java
@@ -1,6 +1,23 @@
 public class Practice {
     public static boolean pangram(String str){
+        int n=str.length();
 
+        int[] fre=new int[26];
+
+        for(int i=0; i<n; i++){
+            char ch=str.charAt(i);
+
+            int idx=ch-'a';
+            fre[idx]++;
+        }
+
+        for(int i=0; i<26; i++){
+            if(fre[i]==0){ // ith character is not there in my string 
+                return false;
+            }
+        }
+
+        return true;
     }
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
