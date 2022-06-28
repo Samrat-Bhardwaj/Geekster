b57257c68cf178673785df9bc3af492af37b2a1f
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2022-06-28 21:25:00 +0530

    28 june

--- Strings/28_6_2022/Practice.java
+++ Strings/28_6_2022/Practice.java
@@ -1,6 +1,58 @@
 import java.util.*;
 
 public class Practice {
+    public static boolean checkBalanced(String str){
+        int n=str.length();
+        int first_half=0;
+        int second_half=0;
+
+        if(n%2==0){
+            // calculating first half
+            for(int i=0; i<n/2; i++){
+                char ch=str.charAt(i);
+
+                int val=ch-'0';
+                first_half=first_half+val;
+            }
+
+            // calculating second half
+            for(int i=n/2; i<n; i++){
+                char ch=str.charAt(i);
+
+                int val=ch-'0';
+                second_half=second_half+val;
+            }
+        } else { // in case of odd length string 
+            // calculating first half
+            for(int i=0; i<n/2; i++){
+                char ch=str.charAt(i);
+
+                int val=ch-'0';
+                first_half=first_half+val;
+            }
+
+            // calculating second half
+            for(int i=n/2 + 1; i<n; i++){
+                char ch=str.charAt(i);
+
+                int val=ch-'0';
+                second_half=second_half+val;
+            }  
+        }
+
+        if(first_half==second_half){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int stringToInteger(String str){
+        // convert the input "str" to "integer"
+        int ans=Integer.parseInt(str);
+        return ans;
+    } 
+
     public static boolean isVowel(char ch){
         if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
             return true;
@@ -14,42 +66,45 @@ public class Practice {
 
         String str=scn.nextLine();
 
-        int n=str.length();
-        int first_occurrence=-1;
-        int last_occurrence=-1;
+        // int n=str.length();
+        // int first_occurrence=-1;
+        // int last_occurrence=-1;
 
-        int last=-1;
-        int min_distance=n+1;
+        // int last=-1;
+        // int min_distance=n+1;
 
-        for(int i=0; i<n; i++){
-            char ch=str.charAt(i);
+        // for(int i=0; i<n; i++){
+        //     char ch=str.charAt(i);
 
-            if(isVowel(ch)==true){
-                if(first_occurrence==-1){ // i have not yet dicovered any vowel
-                    first_occurrence=i;
-                }
+        //     if(isVowel(ch)==true){
+        //         if(first_occurrence==-1){ // i have not yet dicovered any vowel
+        //             first_occurrence=i;
+        //         }
 
-                last_occurrence=i;
+        //         last_occurrence=i;
 
-                // finding min distance
-                if(last==-1){
-                    last=i;
-                } else {
-                    int distance = i - last;
-                    // min_distance=Math.min(min_distance,distance);
+        //         // finding min distance
+        //         if(last==-1){
+        //             last=i;
+        //         } else {
+        //             int distance = i - last;
+        //             // min_distance=Math.min(min_distance,distance);
 
-                    if(distance < min_distance){
-                        min_distance=distance;
-                    }
+        //             if(distance < min_distance){
+        //                 min_distance=distance;
+        //             }
 
-                    last=i;
-                }
-            }
-        }
+        //             last=i;
+        //         }
+        //     }
+        // }
 
-        int max_distance=last_occurrence-first_occurrence;
+        // int max_distance=last_occurrence-first_occurrence;
 
-        System.out.println("Minimum distance between two vowels is: "+min_distance);
-        System.out.println("Maximum distance between two vowels is: "+max_distance);
+        // System.out.println("Minimum distance between two vowels is: "+min_distance);
+        // System.out.println("Maximum distance between two vowels is: "+max_distance);
+
+        int ans=stringToInteger(str);
+        System.out.println(ans);
     }
 }
