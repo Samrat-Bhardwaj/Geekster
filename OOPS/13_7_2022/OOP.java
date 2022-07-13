22e53c88df234a790fb28ec230dc4288b2490280
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2022-07-13 22:05:12 +0530

    OOPS

--- OOPS/13_7_2022/OOP.java
+++ OOPS/13_7_2022/OOP.java
@@ -7,20 +7,59 @@ class Car {
     }
 }
 
+class Student {
+    private String name="Samrat";
+    int roll_num=100;
+    int age=25;
+
+    // default constructor 
+    public Student(){
+        System.out.println("This is constructor 1");
+    }
+
+    public Student(int new_roll_number, int new_age){
+        System.out.println("This is constructor 2");
+        roll_num=new_roll_number;
+        age=new_age;
+    }
+
+    public Student(int new_roll_number){
+        System.out.println("This is constructor 3");
+        roll_num=new_roll_number;
+    }
+
+    
+    public void getName(){
+        System.out.println("Name is "+ name);
+    }
+
+    public void setName(String newName){
+        name=newName;
+    }
+
+    public void add(int a, int b){
+        System.out.println("Result of addition is " + (a+b));
+    }
+}
+
 public class OOP {
     public static void main(String[] args) {
-        // className objectName = "new" + "ClassName"+ "()";
-        Car c1 = new Car();
+            // className objectName = "new" + "ClassName"+ "()";
 
-        c1.color = "Blue";
-        c1.model_number = 12334;
+        Student s1=new Student();
+        Student s2=new Student(21);
+        Student s3=new Student(25);
+
+        Student s4=new Student(123,15425);
+
+        Student s5=new Student(123,15425);
+
+        // Student geek=new Student(1200,12);
+        
 
 
-        System.out.println(c1.color);
-        System.out.println(c1.model_number);
+        // System.out.println(" Roll number is "+geek.roll_num+" and age is "+geek.age);
 
-        c1.doNothing();
-
-        System.out.println(c1);
+        // geek.add(4,7);
     }
 }
---
+++ OOPS/13_7_2022/OOP2.java
@@ -0,0 +1,3 @@
+public class OOP2 {
+    
+}
