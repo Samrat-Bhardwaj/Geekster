29b6a629648102dbdd2756711836b4992301e232
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2022-07-20 22:04:55 +0530

    20 july

--- Collections/ArrayList/Implementation.java
+++ Collections/ArrayList/Implementation.java
@@ -22,7 +22,7 @@ class ArrayList {
 
     public void add(int val){
         if(size==data.length){
-            doubleTheSpace();
+            this.doubleTheSpace();
         }
 
         data[size]=val;
@@ -30,7 +30,7 @@ class ArrayList {
     }
 
     public int get(int idx)  {
-        if(idx>=size){
+        if(idx<0 || idx>=size){
             System.out.println("Array index out of bound");
             return -1;
         }
@@ -39,7 +39,7 @@ class ArrayList {
     }
 
     public void set(int idx, int val){
-        if(idx>=size){
+        if(idx<0 || idx>=size){
             System.out.println("Array index out of bound");
             return;
         }
@@ -52,14 +52,22 @@ class ArrayList {
     }
 
     public void remove(int idx){
+        if(idx<0 || idx>=size){
+            System.out.println("Array index out of bound");
+            return;
+        }
 
+        for(int i=idx; i<size; i++){
+            data[i]=data[i+1];
+        }
+
+        size--;
     }
 }
 
 public class Implementation {
     public static void main (String[] args) {
         ArrayList al=new ArrayList();
-        
 
         for(int val=2; val<=10; val=val+2){
             al.add(val);
@@ -70,8 +78,14 @@ public class Implementation {
             System.out.println(val);
         }
 
-        al.set(3,45);
+        al.set(14,45);
 
-        System.out.println(al.get(3));
+        // System.out.println(al.get(3));
+        al.remove(1);
+
+        for(int i=0; i<al.size(); i++){
+            int val=al.get(i);
+            System.out.println(val);
+        }
     }
 }
