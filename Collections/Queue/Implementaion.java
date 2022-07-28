1b30b3a5a5ae06b7110ac6af12b9ad4b5644c8e2
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2022-07-28 21:03:04 +0530

    Stack implementation

---
+++ Collections/Queue/Implementaion.java
@@ -0,0 +1,27 @@
+class Queue {
+    private int[] data;
+
+    public Queue(){
+        
+    }
+
+    public void add(int ele){
+
+    }
+
+    public int remove(){
+
+    }
+
+    public int peek(){
+
+    }
+
+    public int size(){
+
+    }
+}
+
+public class Implementaion {
+    
+}
