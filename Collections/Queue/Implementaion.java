e987e8fb3eab7fffcf17d9c0d1f644ce9732c245
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2022-07-28 21:44:27 +0530

    Queue implementation

--- Collections/Queue/Implementaion.java
+++ Collections/Queue/Implementaion.java
@@ -1,24 +1,57 @@
 class Queue {
     private int[] data;
+    private int front;
+    private int size;
 
     public Queue(){
-        
+        this.data=new int[4];
+        this.front=0;
+        this.size=0;
     }
 
     public void add(int ele){
+        if(size==data.length){
+            doubleSize();
+        }
 
+        int idx = (front + size) % data.length;
+        data[idx]=ele;
+
+        size++;
     }
 
     public int remove(){
+        if(size==0){
+            System.out.println("Runtime error!!, Queue is empty");
+            return -1;
+        }
 
+        int ele=data[front];
+        
+        front=(front+1) % data.length;
+
+        // if(front==data.length) {
+        //     front=0;
+        // }
+
+        size--;
+
+        return ele;
     }
 
     public int peek(){
+        if(size==0){
+            System.out.println("Runtime error!!, Queue is empty");
+            return -1;
+        }
 
+        int ele=data[front];
+        
+        return ele;
     }
 
     public int size(){
-
+        return this.size;
     }
 }
 
