c3b7f5c5d710d837ff0c3697e97a55e16c78f3e6
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2022-08-09 21:41:35 +0530

    LinkedList implementation

--- LinkedList/Implementation.java
+++ LinkedList/Implementation.java
@@ -15,9 +15,17 @@ class Node {
 
 public class Implementation {
     public static void printLinkedList(Node head){
-        
+        System.out.println("Printing linked list");
+        Node itr=head;
+
+        while(itr!=null){
+            System.out.print(itr.data+" ");
+
+            Node nbr=itr.next;
+            itr=nbr;
+        }
     }
-    
+
     public static void main(String[] args) {
         // Node head=new Node(5);
 
@@ -36,21 +44,19 @@ public class Implementation {
         System.out.println("Enter number of inputs ");
         int n=scn.nextInt();
 
-        Node head=null;
-        Node itr=null;
-        for(int i=0; i<n; i++){
+        int first_ele=scn.nextInt();
+
+        Node head=new Node(first_ele);
+        Node itr=head;
+
+        for(int i=1; i<n; i++){
             int data=scn.nextInt();
 
-            if(i==0){
-                head=new Node(data);
-            } else if(i==1){
-                itr=new Node(data);
-                head.next=itr;
-            } else {
-                Node n1=new Node(data);
-                itr.next=n1;
-                itr=itr.next;
-            }
+            Node n1=new Node(data);
+            itr.next=n1;
+
+            Node nbr=itr.next;
+            itr=nbr;
         }
 
         printLinkedList(head);
